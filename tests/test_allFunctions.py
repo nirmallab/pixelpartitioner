2fe7b3a8fb22900cc944b67d02f01dfda0caa95b
Ajit Johnson Nirmal <ajitjohnson.n@gmail.com>
2024-03-03 16:37:01 -0500

    PixelPartitioner test typo

--- tests/test_allFunctions.py
+++ tests/test_allFunctions.py
@@ -6,7 +6,7 @@ Created on Sun Mar  3 15:56:57 2024
 """
 
 from pixelpartitioner.getImages import getImages
-from pixelpartitioner.pixelpartitioner import pixelpartitioner
+from pixelpartitioner.PixelPartitioner import PixelPartitioner
 from pixelpartitioner.plotPixel import plotPixel
 import os
 
